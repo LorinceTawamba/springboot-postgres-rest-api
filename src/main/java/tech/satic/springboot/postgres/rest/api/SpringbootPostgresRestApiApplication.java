3a9e69e13f55fd646b92f60c3bf13f916ae14926
Lorince TAWAMBA <lorince.tawamba@gmail.com>
2022-09-25 15:10:42 +0100

    Initial commit

---
+++ src/main/java/tech/satic/springboot/postgres/rest/api/SpringbootPostgresRestApiApplication.java
@@ -0,0 +1,13 @@
+package tech.satic.springboot.postgres.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgresRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPostgresRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/satic/springboot/postgres/rest/api/SpringbootPostgresRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.satic.springboot.postgres.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostgresRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
